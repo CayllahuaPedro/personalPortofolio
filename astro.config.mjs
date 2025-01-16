a98d774a399e6ac8cbe7b9017240e132abe0e54e
pedroCayllahua <pedro@iantech.com.ar>
2025-01-16 17:43:55 -0300

    add Tailwind and React integrations; create Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
+  integrations:[react(), tailwind()],
   // Remove any assets configuration as it's not supported
   // Use basic configuration only
 });
