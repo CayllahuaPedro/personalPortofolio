966b8041b17d5d71657e970a834dbfe73d190635
pedroCayllahua <pedro@iantech.com.ar>
2025-01-16 17:52:13 -0300

    add Netlify SSR adapter and middleware for image processing

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,30 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_42LjB_Kw.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/.pnpm/astro@4.16.18_rollup@4.30.1_typescript@5.7.3/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "5318b041-3240-4aa9-9106-6afa3c46ac0c"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
